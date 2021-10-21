6bd3ddc4392b825b7f738a1b4a6880b0b886b9ad
Ted <Ted>
2021-10-21 11:50:16 +0800

    Fetch infos

--- main.py
+++ main.py
@@ -24,19 +24,35 @@ class Repo:
         commit = C
         issue = I
 
-
 for it in soup.find_all(itemprop="name codeRepository"):
     newbrowser = webdriver.Chrome()
-    newbrowser.get(domain + it.attrs['href'])
+    newdomain = domain + it.attrs['href']
+    newbrowser.get(newdomain)
     newpage_Source = newbrowser.page_source
     newsoup = BeautifulSoup(newpage_Source, "lxml")
-    D = newsoup.find(class_="f4 mt-3").string
-    N = newsoup.find(itemprop="name").children.string
-    Li = newsoup.find(attrs={"href" : re.compile(r'$LICENSE')})
-    print (D)
-    print (N)
-    print (Li)
+    D = newsoup.find(class_="f4 mt-3").text
+    N = newsoup.find(itemprop="name").contents[1].text
+    Li = newsoup.find(attrs={"href" : re.compile('LICENSE$')}).text
+    Lan=[]
+    for i in newsoup.find_all(class_="color-fg-default text-bold mr-1"):
+        Lan.append([i.text, i.find_next_sibling().text])
+    
+    nextbrowser = webdriver.Chrome()
+    nextdomain = newdomain + '/commits'
+    nextbrowser.get(nextdomain)
+    nextpage_Source = nextbrowser.page_source
+    nextsoup = BeautifulSoup(nextpage_Source, "lxml")
+    for i in nextsoup.find_all(class_="mb-1"):
+        print(i.text)
+        for j in i.find_next_sibling().find_all(class_="commit-author user-mention"):
+            print(j)
+        break
+    nextbrowser.close()
+    #print (D)
+    #print (N)
+    #print (Li)
+    #print (Lan)
+    
     newbrowser.close()
     break
-browser.close
-()
+browser.close()
